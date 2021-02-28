139897157f94046116751368dceeff9bf0cfbb9a
yatsun <yatsunlam25@gmail.com>
2021-02-28 00:46:22 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.each{|ele|}.join
+    elsif array.length == 2
+        array.each{|ele|}.join(" and ")
+    elsif array.length == 3
+        array.each{|ele|}.join(", ").insert(-10, "and ")
+    else 
+        temp = array.pop
+        array << "and " + temp
+        array.join(", ")
+        # return array
+  #    temp = array.pop
+  #    array << "and "
+  #    array.join
+  #    array << temp    
+    end
 end
